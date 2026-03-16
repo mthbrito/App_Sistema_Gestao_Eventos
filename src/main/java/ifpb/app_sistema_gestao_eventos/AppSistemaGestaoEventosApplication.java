24cdeceb48eaf26f4fe2a71602aa93aca36bbd43
Matheus Brito <matheus.sbrito14@gmail.com>
2026-03-16 20:51:18 -0300

    Primeiro commit

---
+++ src/main/java/ifpb/app_sistema_gestao_eventos/AppSistemaGestaoEventosApplication.java
@@ -0,0 +1,13 @@
+package ifpb.app_sistema_gestao_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSistemaGestaoEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSistemaGestaoEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ifpb/app_sistema_gestao_eventos/AppSistemaGestaoEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.app_sistema_gestao_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSistemaGestaoEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
